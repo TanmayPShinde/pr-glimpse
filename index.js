87ece283f6f3c95a85a7f2176b9fc5c4098905be
TanmayPShinde <tps.shinde@gmail.com>
2023-10-28 20:48:21 +0530

    info: added comments

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ module.exports = (app) => {
       context.payload.pull_request;
     app.log.info("pr opened");
 
+    // fetched commits and comments on the PR
     Promise.all([fetch(commits_url), fetch(comments_url)])
       .then((responses) =>
         Promise.all(responses.map((response) => response.json()))
@@ -23,9 +24,9 @@ module.exports = (app) => {
       .then(([commits, comments]) => {
         const commitMsgs = commits.map((elem) => elem.commit.message);
         const commentMsgs = comments.map((elem) => elem.body);
-
         commentMsgs.push(title, body);
 
+        // checking the mesasges for /execute command
         if (isPresent_exec_cmd(commitMsgs) || isPresent_exec_cmd(commentMsgs)) {
           app.log("has work");
 
@@ -39,12 +40,15 @@ module.exports = (app) => {
             },
           } = context.payload.pull_request;
 
+          // fetching code diff in the PR from Octokit api
           const data = getCodeDiffInPR({ context, owner, repo, pull_number });
           data.then((diff) => {
+            // getting description for changes in PR
             const resp = getOpenAi_PR_explanation(diff);
             resp.then((data) => {
               app.log(data);
 
+              // sending back the response with an added description as a comment
               const issueComment = context.issue({
                 body: data,
               });
